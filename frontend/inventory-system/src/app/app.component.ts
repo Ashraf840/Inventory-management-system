bba73bf596334dac507ff7537e343679366ffd85
Ashraf840 <shift2unleashed2011@gmail.com>
2024-06-02 22:54:03 +0530

    Set the dropdown items in side nav bar

--- frontend/inventory-system/src/app/app.component.ts
+++ frontend/inventory-system/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'inventory-system';
+
+  constructor(private el:ElementRef) {}
+  
+  ngOnInit(): void {
+    
+  }
+
+  toggleDropdown(val: string) {
+    console.log("Clicked dropdown:", val);
+    let drpElem = document.querySelector(`.dropdown-container-${val}`);
+    console.log("drpElem:", drpElem);
+    drpElem?.classList.toggle('show');
+  }
 }
